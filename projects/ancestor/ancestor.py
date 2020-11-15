57f607499db7f8dafb2e1c872c0bae0a4c11b46b
Conary36 <cbeckford144@gmail.com>
2020-11-15 15:10:03 -0500

    ...

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,19 +1,28 @@
 def earliest_ancestor(ancestors, starting_node):
-  '''
+    '''
    Write a function that, given the dataset and the ID of an individual in the dataset, returns their earliest known
    ancestor – the one at the farthest distance from the input individual.If there is more than one ancestor tied
   for "earliest", return the one with the lowest numeric ID.If the input individual has no parents, the function
   should return -1.
   '''
-  visitid = set()
+
+    visited = []
+    for i in get_parents(ancestors,starting_node):
+        visited.append(i)
+    while len(visited) > 0:
+        node = visited.pop(0)
+        for i in get_parents(ancestors,node):
+            visited.append(i)
+        if len(visited) == 0:
+            return node
+
+    return -1
 
 
-
-
-def get_ancestors(dataset, children):
-
-
-
+def get_parents(dataset, children):
+    parents = [node[0] for node in dataset if node[1] == children]
+    parents.sort(reverse= True)
+    return parents
 
 
 '''
